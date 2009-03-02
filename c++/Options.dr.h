2665b20b25567be776a0a237332df22c8062d794
daemonraco <daemonraco@3a7a90e2-0520-11de-a1f0-819f45317607>
2009-03-02 02:51:33 +0000

    Adding codes (note: this revision doesn't compile)

---
+++ c++/Options.dr.h
@@ -0,0 +1,48 @@
+/**
+ * @file Options.dr.h
+ * @date 2009-03-01
+ * @author Alejandro Darío Simi
+ */
+
+/**
+ *
+ * NextTools - An amount of usefull functions.
+ * Copyright (C) 2009 Alejandro Darío Simi
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef OPTIONS_DR_H_
+#define OPTIONS_DR_H_
+
+namespace dr {
+
+/**
+ * @todo documentar
+ * @brief Options is a manager for parameters.
+ */
+class Options {
+	public:
+		Options();
+		virtual ~Options();
+
+	public:
+		virtual bool load(int counter, char** params);
+};
+
+}
+
+#endif /* OPTIONS_DR_H_ */
